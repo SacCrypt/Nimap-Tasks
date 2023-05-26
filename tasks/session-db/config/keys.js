d1c34662d94a62ecae156fa3028bfd371d5d21bf
SacCrypt <59285305+SacCrypt@users.noreply.github.com>
2023-05-26 05:21:37 -0700

    Add files via upload

---
+++ tasks/session-db/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google: {
+        clientID: "68079333705-469c9ua4164ah87elnqa58a9k4o64jr1.apps.googleusercontent.com",
+        clientSecret: "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"
+    }
+}
