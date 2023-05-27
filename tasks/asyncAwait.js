3fc971d65d6455f974babc1a09381c7fec27ad16
SacCrypt <59285305+SacCrypt@users.noreply.github.com>
2023-05-27 17:51:23 +0530

    Delete tasks directory

--- tasks/asyncAwait.js
+++
@@ -1,7 +0,0 @@
-console.log("Before Calling the Function");
-const asyncFunction = async () => {
-  const response = await fetch("https://google.com");
-  console.log(response);
-};
-asyncFunction();
-console.log("Should be printed before response object");
--- tasks/handleCallback.js
+++
@@ -1,9 +0,0 @@
-const fs = require("node:fs");
-
-fs.readFile(__dirname + "/misc/file1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
